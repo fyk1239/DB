eb7437e502b232b5298dfaa73da52d315405079d
fyk <2788745314@qq.com>
2022-12-14 19:58:21 +0800

    完成functions.py所有修改

--- app01/functions.py
+++ app01/functions.py
@@ -79,15 +79,19 @@ def search_grade_from_id(Cursor, Sno, Cno):  # 通过课程号查找某学生的
     #Cursor.execute("select * from app01_student natrual join grade where Sno='"+Sno+"' and Cno like '%"+Cno+"%'")
 
 
-def search_grade_from_name(Cursor, Sno, Cname):  # 通过课程名查找某学生的成绩
-    Cursor.execute("select \"Cno_id\",\"Gscore\" from public.app01_student join public.app01_grade on app01_student.\"Sno\"=app01_grade.\"Sno\" where \"app01_student.Sno\"='" +
-                   Sno+"' and \"Cname\" like'%"+Cname+"%'")
+def search_grade_from_name(Cursor, Sno, Cname):  # 通过课程名查找某学生的成绩，修改完成
+    # 把课程名转换为课程号
+    Cursor.execute(
+        "select \"Cno\" from public.app01_course where \"Cname\"='"+Cname+"'")
+    Cno = Cursor.fetchone()[0]
+    Cursor.execute("select public.app01_grade.\"Cno_id\", public.app01_grade.\"Gscore\" from public.app01_student join public.app01_grade on app01_student.\"Sno\"=app01_grade.\"Sno_id\" where app01_student.\"Sno\"='" +
+                   Sno+"' and \"Cno_id\" like'%"+Cno+"%'")
     return Cursor.fetchall()
 
 
-def get_app01_course_announcement(Cursor, Cno):  # 通过课程号查找课程公告
+def get_course_announcement(Cursor, Cno):  # 通过课程号查找课程公告，修改完成
     Cursor.execute(
-        "select * from public.app01_announcement where \"Cno\"='"+Cno+"'")
+        "select * from public.app01_announcement where \"Cno_id\"='"+Cno+"'")
     return Cursor.fetchall()
 
 
@@ -108,15 +112,14 @@ def get_teacher_app01_course(Cursor, Tno):  # 查找教师教授的课
     return Cursor.fetchall()
 
 
-def publish_announcement(Cursor, Cno, announcement_content):  # 添加课程公告
-    Cursor.execute("select \"count(*)\" from public.app01_announcement ")
-    Ano = Cursor.fetchone()[0]
-    Ano = str(Ano)
+def publish_announcement(Cursor, Cno, announcement_content):  # 添加课程公告，修改完成
+    Cursor.execute("select count(*) from public.app01_announcement ")
+    Ano = Cursor.fetchone()[0] + 40001  # 生成公告号
     Cursor.execute("insert into public.app01_announcement values('" +
-                   Ano+"','"+announcement_content+"'")
+                   str(Ano)+"','"+announcement_content+"','"+Cno+"')")
 
 
-def update_grade(Cursor, Sno, Cno, grade):  # 修改学生成绩，修改成功
+def update_grade(Cursor, Sno, Cno, grade):  # 修改学生成绩，修改完成
     Cursor.execute("update public.app01_grade set \"Gscore\"="+grade +
                    " where \"Sno_id\"='"+Sno+"' and \"Cno_id\"='"+Cno+"'")
 
@@ -126,13 +129,12 @@ if __name__ == "__main__":
     cur = conn.cursor()  # 创建会话
     # print(search_stu(cur, '00001'))
     # print(get_grade(cur, '00002'))
-    # f_one=search_app01_course_from_id(cur,'000000001','1')
-    # print(f_one)
-    # publish_announcement(cur,'000001',"测试公告")
-    # print(time.asctime( time.localtime(time.time()) ))
-    update_grade(cur, '00001', '20001', '99')
-    update_stu_info(cur, '00001')
+    # publish_announcement(cur,'20001',"测试公告")
+    # print(get_course_announcement(cur, '20001'))
+    # update_grade(cur, '00001', '20001', '99')
+    # update_stu_info(cur, '00001')
     # print(update_stu_info(cur, '00001'))
     # print(search_grade_from_id(cur, '00001', '20001'))
-    print(search_grade_from_name(cur, '00001', '计算机网络'))
+    print(search_grade_from_name(cur, '00001', '数据库系统原理'))
+    print(search_teacher_num(cur, '10001'))
     close_db_connection(conn)  # 关闭数据库连接
